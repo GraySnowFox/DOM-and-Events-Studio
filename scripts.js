f881fb135c90273363eff8ce663fe7925a506970
GraySnowFox <78774887+GraySnowFox@users.noreply.github.com>
2021-04-23 18:58:38 -0500

    All the steps up until 5

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ window.addEventListener('load', function() {
     shutBackground = document.getElementById("shuttleBackground");
     shutHeight = document.getElementById("spaceShuttleHeight");
     takeOffButton = document.getElementById("takeoff");
+    landButton = document.getElementById("landing");
+    abortButton = document.getElementById("missionAbort");
 
     takeOffButton.addEventListener("click", function(){
         if(window.confirm("Confirm that the shuttle is ready for takeoff.") === true){
@@ -15,8 +17,34 @@ window.addEventListener('load', function() {
             let shuttleLift = parseInt(shutHeight.innerHTML);
             shuttleLift += 10000;
             shutHeight.innerHTML = shuttleLift;
-
         };
+
+        landButton.addEventListener("click", function(event){
+          
+            flightStatusText.innerHTML = "The shuttle has landed.";
+            shutBackground.style.backgroundColor = "green";
+            shutHeight.innerHTML = 0;
+            window.confirm("The shuttle has landed");
+            event.stopPropagation();
+
+        });
+
+        abortButton.addEventListener("click", function(){
+           if(window.confirm("Confirm that you want to abort the mission.") === true){
+               flightStatusText.innerHTML = "Mission Aborted";
+               shutBackground.style.backgroundColor = "green";
+               shutHeight.innerHTML = 0;
+           };
+        })
+
+
+
+
+
+
+
+
+
     });
 
 
