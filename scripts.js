0b37e3dc9ae32cff17183a358e4a93f68cfccb0a
GraySnowFox <78774887+GraySnowFox@users.noreply.github.com>
2021-04-23 18:28:29 -0500

    First steps in studio

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
-// Remember to pay attention to page loading!
+
+
+window.addEventListener('load', function() {
+
+    flightStatusText = document.getElementById("flightStatus");
+    shutBackground = document.getElementById("shuttleBackground");
+    shutHeight = document.getElementById("spaceShuttleHeight");
+    takeOffButton = document.getElementById("takeoff");
+
+    takeOffButton.addEventListener("click", function(){
+        if(window.confirm("Confirm that the shuttle is ready for takeoff.") === true){
+            flightStatusText.innerHTML = "Shuttle in Flight";
+            shutBackground.style.backgroundColor = "blue";
+            let shuttleLift = parseInt(shutHeight.innerHTML);
+            shuttleLift += 10000;
+            shutHeight.innerHTML = shuttleLift;
+
+        };
+    });
+
+
+  });
